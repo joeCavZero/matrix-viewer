47478fd02c7cbb6553b9cef65d9b8a9e2457b4df
JoeCavZero <joaocavalcantejcavalcante@gmail.com>
2024-09-14 12:47:16 -0300

    [refac] the triangular matrix view now display the N used

--- src/engine/scene/views/triangle_matrix.py
+++ src/engine/scene/views/triangle_matrix.py
@@ -13,8 +13,8 @@ class TriangleMatrixScene( Scene ):
         from src.engine.widget.label.label import Label
         self.add_widgets( 
             RedirectButton((20,20), "voltar", "back", self.engine),
-            Label((150,70),"Matriz Triangular Superior", self.engine),
-            Label((700,70),"Matriz Triangular Inferior", self.engine),
+            Label((150,70),f"Matriz Triangular Superior N={self.n}", self.engine),
+            Label((700,70),f"Matriz Triangular Inferior N={self.n}", self.engine),
             TriangularMatrixWidget((150,120), (500,500), self.n, "upper", self.engine),
             TriangularMatrixWidget((700,120), (500,500), self.n, "lower", self.engine)
         )
